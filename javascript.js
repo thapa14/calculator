48c585f182453e6de0221b1224824c8f6880c7cf
thapa14 <rajthapa081@gmail.com>
2022-05-20 12:13:38 +0530

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,50 @@
+const result = document.getElementById('display');
+
+function display(id) {
+    let button = document.getElementById(id);
+    let buttonNumber = button.innerText;
+    if (result.innerText.length < 17) {
+        if (result.innerText == '0') result.innerText = buttonNumber;
+        else result.innerText += buttonNumber;
+    }
+    else {
+        window.alert('maximum size occupied');
+    }
+}
+
+function symbols(id) {
+    let initial = result.innerText;
+    let button = document.getElementById(id);
+    let buttonNumber = button.innerText;
+    let arr = result.innerText.split("");
+    let lastNumber = arr.slice(-1);
+    if (result.innerText.length < 17) {
+        if (lastNumber == '+' || lastNumber == '-' || lastNumber == '*' || lastNumber == '/' || lastNumber == '.') {
+            arr.pop();
+            arr.push(button.innerText);
+            result.innerText = arr.join("");
+        }
+        else {
+            if (initial == '0') {
+                if (buttonNumber == '+' || buttonNumber == "-") {
+                    result.innerText = buttonNumber;
+                }
+                else {
+                    result.innerText = `0${buttonNumber}`;
+                }
+            }
+            else result.innerText += buttonNumber;
+        }
+    }
+}
+
+function calculate() {
+    let numbers = result.innerText;
+    let total = eval(numbers);
+    total = total.toString();
+    result.innerText = total.slice(0,17);
+}
+
+function clearScreen() {
+    result.innerText = "0";
+}
